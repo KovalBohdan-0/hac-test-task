cf7725897d4a966840f12028fc168e8f12b24298
Bohdan Koval <kovalbohdan96@gmail.com>
2024-07-03 22:17:14 +0300

    Added API documentation

--- src/main/java/org/rest/hactesttask/csvdata/CSVDataController.java
+++ src/main/java/org/rest/hactesttask/csvdata/CSVDataController.java
@@ -1,5 +1,11 @@
 package org.rest.hactesttask.csvdata;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+@Tag(name = "CSV Data")
 @RestController
 @RequestMapping("/api/csv")
 public class CSVDataController {
@@ -19,6 +26,13 @@ public class CSVDataController {
         this.csvDataService = csvDataService;
     }
 
+    @Operation(summary = "Upload a CSV file and save data")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "File uploaded and data saved successfully",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = String.class)) }),
+            @ApiResponse(responseCode = "500", description = "Error processing file",
+                    content = @Content) })
     @PostMapping("/upload")
     public ResponseEntity<String> uploadCSV(@RequestParam("file") MultipartFile file) {
         try {
--- src/test/java/org/rest/hactesttask/HacTestTaskApplicationTests.java
+++ src/test/java/org/rest/hactesttask/HacTestTaskApplicationTests.java
@@ -1,13 +1,9 @@
 package org.rest.hactesttask;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class HacTestTaskApplicationTests {
-
     @Test
     void contextLoads() {
     }
-
 }
